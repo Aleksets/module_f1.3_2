0e618acfce1d46bcb5663ba3e26303b053bdd207
Alexey Zaplatin <a.g.zaplatin@mail.ru>
2024-05-21 00:00:00 +0300

    Commit 5 - After f1.4 video 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import "../styles/Header.css"
 
-let buttonName = "Some button";
-const handleClick = () => {
-    console.log("you pushed the button", buttonName)
-}
 
-class Header extends Component {
-    render() {
-        return (
-            <header>This is header
-                <button className={ "some-button" } onClick={ handleClick }>{ buttonName }</button>
-            </header>
-        )
+function Header() {
+    let buttonName = "Some button";
+    let [count, setNewCount] = useState(0);
+    const handleClick = () => {
+        setNewCount(count + 1);
     }
+   
+    return (
+        <header>This is header
+            <button className={ "some-button" } onClick={ handleClick }>{ buttonName } clicked { count } times</button>
+        </header>
+    )
 }
 
 export default Header;
